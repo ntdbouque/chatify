f65fc4cf71274aa86a0fed4e6fae928721277ba4
Nguyen Truong Duy <duyngyn12.0404@gmail.com>
2025-11-25 11:29:19 +0700

    update route login/logout in auth.routes.js

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,14 +1,12 @@
 import express from 'express';
-import { signup } from '../controller/auth.controller.js';
+import { signup, login, logout } from '../controller/auth.controller.js';
 
 const router = express.Router();
 router.post('/signup', signup);
 
-router.get('/login', (req, res) => {
-  res.json({ message: 'Login endpoint' });
-});
+router.post('/login', login);
 
-router.get('/logout', (req, res) => {
-  res.json({ message: 'Logout endpoint' });
-});
+router.post('/logout', logout);
+
+
 export default router;
