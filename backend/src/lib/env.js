5a9103c62264b40c7527bb7e9e1a45b0d8b9cd69
Nguyen Truong Duy <duyngyn12.0404@gmail.com>
2025-11-26 12:32:58 +0700

    Rate-Limit-and-Bot-Detect-Implementation

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -8,5 +8,7 @@ export const ENV = {
   CLIENT_URL: process.env.CLIENT_URL,
   RESEND_API_KEY: process.env.RESEND_API_KEY,
   EMAIL_FROM: process.env.EMAIL_FROM,
-  EMAIL_FROM_NAME: process.env.EMAIL_FROM_NAME
+  EMAIL_FROM_NAME: process.env.EMAIL_FROM_NAME,
+  ARCJET_KEY: process.env.ARCJET_KEY,
+  ARCJET_ENV: process.env.ARCJET_ENV,
 };
